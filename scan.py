0a158cabea942cc3a65e3163ee95923435fd2cef
Jose Velasco <al19760627@ite.edu.mx>
2024-10-25 22:43:51 -0700

    replace

--- scan.py
+++ scan.py
@@ -13,21 +13,38 @@ def ping_device(ip):
 
 @app.route('/')
 def index():
-    network_to_scan = '40.10.7.254/21'  
+    network_to_scan = '140.10.7.1/21'  
     nm = nmap.PortScanner()
     scan_result = nm.scan(hosts=network_to_scan, arguments='-sn')
     
- 
     active_devices = []
+    active_count = 0
+    inactive_count = 0
+
     for host in scan_result['scan']:
+        if len(active_devices) >= 25:  
+            break
+
         mac_address = scan_result['scan'][host]['addresses'].get('mac', None)
-        if mac_address: 
+        if mac_address:
+            is_active = ping_device(host)
             active_devices.append({
                 'ip': host,
-                'active': ping_device(host)  
+                'active': is_active
             })
-    
-    return render_template('index.html', devices=active_devices, scan_result=network_to_scan)
+
+            if is_active:
+                active_count += 1
+            else:
+                inactive_count += 1
+
+    return render_template(
+        'index.html',
+        devices=active_devices,
+        scan_result=network_to_scan,
+        active_count=active_count,
+        inactive_count=inactive_count
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
